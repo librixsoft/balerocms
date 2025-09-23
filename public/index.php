f1aeb3f70887f0152c22b595676489f2dd079e24
anibal gomez <balerocms@gmail.com>
2025-09-23 17:21:36 -0600

    note

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Framework\Routing\Router;
 const _CORE_VERSION = "1.0";
 const LOCAL_DIR = __DIR__ . '/..';
 
-define('APP_ENV', 'dev');
+define('APP_ENV', 'dev'); // change to "prod" if you are uploading to your server
 
 require_once LOCAL_DIR . '/bootstrap.php';
 
