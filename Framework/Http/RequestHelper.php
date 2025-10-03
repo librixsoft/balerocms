97bf466668f94040a4f9166e8c412cb58bdb4672
anibal gomez <balerocms@gmail.com>
2025-10-02 22:20:44 -0600

    security

--- Framework/Http/RequestHelper.php
+++ Framework/Http/RequestHelper.php
@@ -71,7 +71,8 @@ class RequestHelper
     {
         $uri = $_SERVER['REQUEST_URI'] ?? '/';
         $path = parse_url($uri, PHP_URL_PATH);
-        return rtrim($path, '/'); // elimina el slash final
+        $finalPath = rtrim($path, '/');
+        return $this->filter($finalPath);
     }
 
 }
